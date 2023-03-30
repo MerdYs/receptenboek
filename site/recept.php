ecfb13e4bf1a824c91cb22357d0d9ae4f9260f09
Mert Babur <mertbabur@hotmail.nl>
2023-03-30 22:49:03 +0200

    COMMIT 2

--- site/recept.php
+++ site/recept.php
@@ -73,7 +73,7 @@ $ingredienten = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <h1>Instructie</h1>
 
                 <h3>Ingrediënten</h3>
-                <p><?php ?></p>
+                <p><?php echo $ingredienten['ingredient_naam'] ?></p>
 
 
                 <h3>Bereidingswijze:</h3>
